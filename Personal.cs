26ed8424e29832b28f211d1555d45ed7d8e3c647
Hernan <hershins7@gmail.com>
2024-05-26 03:53:09 -0300

    Ejercicio-2-realizado

--- Personal.cs
+++ Personal.cs
@@ -1,23 +1,24 @@
 namespace EspacioPersonal
 {
-    public class Empleados
+    public class Empleado
     {
         private string nombre;
-
         private string apellido;
-
         private DateTime nacimiento;
-
         private char estadoCivil;
-
-        private DateTime ingreso;
-
+        private DateTime ingresoEmpresa;
         private double sueldoBasico;
+        private Cargos cargo;
 
-        
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
+        public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+        public DateTime IngresoEmpresa { get => ingresoEmpresa; set => ingresoEmpresa = value; }
+        public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
+        public Cargos CargoEmpleado { get => cargo; set => cargo = value; }
 
-
-        public enum Cargo
+        public enum Cargos
         {
             Auxiliar,        // 0
             Administrativo,  // 1
@@ -26,10 +27,59 @@ namespace EspacioPersonal
             Investigador     // 4
         }
 
-        public Empleados(){
-            
+        public int CalcularAntiguedad(){
+            DateTime Actual  = DateTime.Now; //se crea una fecha actual
+            int ant = Actual.Year - ingresoEmpresa.Year; //se calcula la antiguedad
+            if(ingresoEmpresa.Month > Actual.Month) --ant; //si todavia o es el es del ingrreso a la empresa se resta un año de antiguedad
+            return ant; //se devuelve la antiguedad
+        }
+
+        public int CalculcularEdad(){
+            DateTime Actual  = DateTime.Now; //se crear fecha actual
+            int edad  = Actual.Year - nacimiento.Year; //calculo de edad
+            if(nacimiento.Month > Actual.Month) --edad; //si todavia no es el mes de nacimiento se resta un año a la edad
+            return edad; //se devuelve la edad
+        }
+
+        public int CalcularAniosParaJub(){
+            return (65 - CalculcularEdad()); //se devuelven los años faltantespara jubilarse
+        }
+
+        public double CalcularAdicional(){
+            double adicional = 0;
+            int antiguedad = CalcularAntiguedad();
+
+            if(antiguedad <=20){
+                adicional = sueldoBasico * ((0.01)*antiguedad); //hasta los 20 años el 1 % del sueldo basico por ada año de antiguedad
+            }
+            else
+            {
+                adicional += sueldoBasico * 0.25; //gana un 25% adicional de su sueldo básico independientemente de los años adicionales que siga trabajando. (se fija en un 25%)
+            }
+
+            if(cargo == Cargos.Ingeniero || cargo == Cargos.Especialista){
+                adicional *= 1.50; //el adicional se incrementa en un 50%
+            }
+
+            if(EstadoCivil == 'c' || EstadoCivil == 'C'){
+                adicional+=150000; //si es casado se aumenta $150.000
+            }
+            return adicional; //retorna el adicional
+        }
+
+        public double CalcularSalario(){            
+            return sueldoBasico + CalcularAdicional(); //retorna salario
+        }
+        
+        //metodo constructor
+        public Empleado(string nombre, string apellido, DateTime nacimiento, char estadoCivil, DateTime ingresoEmpresa, double sueldoBasico, Cargos cargo){
+            Nombre = nombre;
+            Apellido = apellido;
+            Nacimiento = nacimiento;
+            EstadoCivil = estadoCivil;
+            IngresoEmpresa = ingresoEmpresa;
+            SueldoBasico = sueldoBasico;
+            CargoEmpleado = cargo;
         }
     }
-
-    
 }
--- Program.cs
+++ Program.cs
@@ -94,8 +94,43 @@ do
 //Ejercicio 2
 
 using EspacioPersonal;
-Empleados[] arregloEmpleados = new Empleados[3];
 
+// arreglo de Empleado
+Empleado[] empleados = new Empleado[] //creo un arrgelo de tipos clase Empleado y reservo memoria apra el mismo a la vez que lo inciialzo, y cada empleado lo cargo con el metodo constructor de empleado ( inicializo el arreglo y crea los objetos al mismo tiempo)
+{
+    new Empleado("Empleado", "Prueba", new DateTime(1960, 10, 26), 'C', new DateTime(2009, 2, 12), 650000, Empleado.Cargos.Ingeniero),//empleados[0]
+    new Empleado("Aquiles", "Bailo", new DateTime(1980, 1, 1), 'C', new DateTime(2010, 1, 1), 650000, Empleado.Cargos.Administrativo),//empleados[1]
+    new Empleado("Yo", "Merengues", new DateTime(1965, 4, 10), 'C', new DateTime(2000, 2, 20), 650000, Empleado.Cargos.Especialista)//empleados[2]
+};
+
+
+double montoTotal = 0;
+int aniosParaJub, menosAnios = 0;
+Empleado emp  = empleados[0];
+
+foreach (var empleado in empleados)
+{
+    montoTotal += empleado.CalcularSalario();
+    aniosParaJub  = empleado.CalcularAniosParaJub();
+    if (aniosParaJub <= menosAnios)
+    {
+        menosAnios = aniosParaJub; //guardo la cantidad de menos años
+        emp = empleado; //guardo los datos del empleado con menos años
+    }
+}
+
+Console.WriteLine("El monto total en concepto de Salarios es: $" + montoTotal);
+Console.WriteLine("Datos de empleado con menos anios para jubilarse: ");
+Console.WriteLine("Nombre y Apellido: " + emp.Nombre + " " + emp.Apellido);
+Console.WriteLine("Nacimiento: "+ emp.Nacimiento);
+Console.WriteLine("Estado Civil (C: casado, S: soltero): "+ emp.EstadoCivil);
+Console.WriteLine("Ingreso a la empresa: " + emp.IngresoEmpresa);
+Console.WriteLine($"Sueldo Basico: $ {emp.SueldoBasico}" );
+Console.WriteLine("Cargo: " + emp.CargoEmpleado);
+Console.WriteLine($"Antiguedad: {emp.CalcularAntiguedad()}");
+Console.WriteLine($"Edad del empleado: {emp.CalculcularEdad()}");
+Console.WriteLine($"Anios que faltan para jubilarse: {emp.CalcularAniosParaJub()}");
+Console.WriteLine($"Salario correspondiente: $ {emp.CalcularSalario()}");
 
 
 
